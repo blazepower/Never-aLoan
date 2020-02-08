4d4d16b418e897c1b3e0e715614dc8831a5a1c7c
Rishik Hombal <hombalrishik@gmail.com>
2020-02-08 00:24:50 -0500

    Adding code

--- src/main/java/Profile/Profile.java
+++ src/main/java/Profile/Profile.java
@@ -1,4 +1,46 @@
 package Profile;
 
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Profile {
+
+    private String fName, lName;
+
+    /**
+     *
+     * @param file
+     * @throws IOException
+     */
+    @PutMapping("/people/profile")
+    public void setProfilePicture(File file) throws IOException {
+        BufferedImage image = ImageIO.read(file);
+        ImageIcon icon=new ImageIcon(image);
+        JFrame frame=new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(200,300);
+        JLabel lbl=new JLabel();
+        lbl.setIcon(icon);
+        frame.add(lbl);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @PutMapping("/people/profile")
+    public void setFirstName(String fName){
+        this.fName = fName;
+    }
+
+    @PutMapping("people/profile")
+    public void setLastName(String lName){
+        this.lName = lName;
+    }
+
 }
--- src/main/java/code/MainApplicationClass.java
+++ src/main/java/code/MainApplicationClass.java
@@ -1,4 +1,11 @@
 package code;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MainApplicationClass {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplicationClass.class, args);
+    }
 }

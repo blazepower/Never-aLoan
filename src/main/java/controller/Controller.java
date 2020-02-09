07da9b10b337fd9c2ac341d591308213ea2d96b6
Rishik Hombal <hombalrishik@gmail.com>
2020-02-09 07:37:03 -0600

    Bug fix

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -116,7 +116,7 @@ public class Controller {
     }
 
     @GetMapping("/projects/{id}")
-    public Project viewProject(@RequestBody Integer id){
+    public Project viewProject(@PathVariable Integer id){
         return projectDatabase.getProject(id);
     }
 
